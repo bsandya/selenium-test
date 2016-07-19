a242a189d3fd75951cbeb7e9d2f36bee2cba89fd
bsandya <sandyabojja@gmail.com>
2016-07-19 12:05:09 +0530

    modified

--- src/Test/SelectAllCheckboxes.java
+++ src/Test/SelectAllCheckboxes.java
@@ -1,8 +1,9 @@
 package Test;
-//import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 //import java.util.List;
 //import org.openqa.jetty.html.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,9 +12,11 @@ public class SelectAllCheckboxes {
 	public static void main(String[] args)
 	{
 		WebDriver driver = new FirefoxDriver();
-		//driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		driver.get("https://www.echoecho.com");
-		//List<WebElement> check=driver.findElements(By.xpath(".//input[@type='checkbox']"));
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+		driver.findElement(By.id("gs_htif0")).sendKeys("selenium", Keys.ENTER);
+		
+		List<WebElement> check=driver.findElements(By.xpath(".//input[@type='checkbox']"));
 	/*int size =check.size();
 		for(size=0;size<size; size++)
 		{
